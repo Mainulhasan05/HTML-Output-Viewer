20ce87330e4b11b906f280d5f8bbb53ca499f7ec
Mainulhasan05 <mdrifatbd5@gmail.com>
2022-12-15 18:11:40 +0600

    msg

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const inputText=document.getElementById("inputText");
+const outputBox=document.getElementById("output")
+let text;
+const renderHTML=()=>{
+    text=inputText.value;
+    outputBox.innerHTML=text;
+}
